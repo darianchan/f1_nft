83f0fa2f143e9f9a0ef1323d5d304fc54886bfcb
Darian <darian.s.chan@gmail.com>
2022-02-03 21:16:59 -0800

    added readme

--- frontend/src/MintSection.jsx
+++ frontend/src/MintSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { ethers } from "ethers";
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -6,11 +6,15 @@ const signer = provider.getSigner();
 
 class MintSection extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state={}
+    this.state = {
+      mintAmount: 1
+    };
 
     this.connectToMetaMask = this.connectToMetaMask.bind(this);
+    this.onMint = this.onMint.bind(this);
+    this.onChangeMintInput = this.onChangeMintInput.bind(this);
   }
 
   async connectToMetaMask() {
@@ -22,13 +26,31 @@ class MintSection extends React.Component {
     }
   }
 
+  // call whitelist mint function / main sale mint function here
+  async onMint() {
+
+  }
+
+  onChangeMintInput(event) {
+    this.setState({mintAmount: event.target.value});
+  }
+
   render() {
-    return(
+    return (
       <div>
         <button onClick={this.connectToMetaMask}>Connect Wallet</button>
+        <br/>
+        <button>Mint</button>
+        <select onChange={this.onChangeMintInput}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
       </div>
-    )
+    );
   }
 }
 
-export default MintSection
+export default MintSection;
